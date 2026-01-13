969a1fde8460513732ba46fb9c0e5d6cd6edfacc
Julie <jackjack170827@gmail.com>
2026-01-12 19:08:28 -0800

    Update weekly meeting location

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -1,7 +1,7 @@
 export const WEEKLY_MEETING = {
   quarter: 'Winter 2026',
   time: 'Fridays 6-8pm',
-  location: 'CSE basement',
+  location: 'CSE basement (B270)',
 }
 
 export const TEAM_SELECTION = {
